71afcf9b453ed4938c3d0948991d022d053ae5e2
zhang-zi-ang <917960894@qq.com>
2020-03-11 21:21:13 +0800

    Demo_01 hello world检查idea和GitHub是否配置正确

---
+++ JavaBasicTest/src/Demo_01.java
@@ -0,0 +1,18 @@
+/**
+ * @program: mygit
+ * @author: zhang-zi-ang
+ * @create: 2020-03-11 21:17
+ * @description: 第一个函数hello world，调试用
+ */
+public class Demo_01 {
+	public static void main(String[] args) {
+		/**
+		* @Description: 主函数
+		* @Param: [args]
+		* @return: void
+		* @Author: zhang-zi-ang
+		* @Date: 2020/3/11
+		*/
+		System.out.println("hello world！");
+	}
+}
--- demo/demo1/demo1.java
+++
@@ -1,7 +0,0 @@
-package demo1;
-
-public class demo1 {
-	public static void main(String[] args) {
-		System.out.println("hello world!");
-	}
-}
