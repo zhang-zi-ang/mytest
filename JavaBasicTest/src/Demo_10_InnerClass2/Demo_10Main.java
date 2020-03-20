fe5b891ee7ef5eb60b41285e89afbd30623f70ca
zhang-zi-ang <917960894@qq.com>
2020-03-20 23:26:13 +0800

    Demo_10 内部类第二部分：局部内部类

---
+++ JavaBasicTest/src/Demo_10_InnerClass2/Demo_10Main.java
@@ -0,0 +1,14 @@
+package Demo_10_InnerClass2;
+
+/**
+ * @program: mygit
+ * @author: zhang-zi-ang
+ * @create: 2020-03-20 22:48
+ * @description: ${description}
+ */
+public class Demo_10Main {
+	public static void main(String[] args) {
+		Outer obj = new Outer();
+		obj.methodOuter();
+	}
+}
