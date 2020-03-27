97d9b858a732882cc4f548d7912040de718af3b9
zhang-zi-ang <917960894@qq.com>
2020-03-27 23:58:08 +0800

    Demo_14 发手气红包案例，添加了支持代码，第一天

--- JavaBasicTest/src/Demo_14_Red/MyRed.java
+++ JavaBasicTest/src/Demo_14_Red/MyRed.java
@@ -1,14 +1,20 @@
 package Demo_14_Red;
 
-import cn.itcast.day11.red.RedPacketFrame;
+import Red.RedPacketFrame;
 
+/**
+ * @program: mygit
+ * @author: zhang-zi-ang
+ * @create: 2020-03-27 23:50
+ * @description: ${description}
+ */
 public class MyRed extends RedPacketFrame {
-    /**
-     * 构造方法：生成红包界面。
-     *
-     * @param title 界面的标题
-     */
-    public MyRed(String title) {
-        super(title);
-    }
+	/**
+	 * 构造方法：生成红包界面。
+	 *
+	 * @param title 界面的标题
+	 */
+	public MyRed(String title) {
+		super(title);
+	}
 }
